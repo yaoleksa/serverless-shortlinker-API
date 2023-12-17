b5343a4259714a23c7fe929b0f74f3d15daf4756
yaoleksa <mryaremchyk@gmail.com>
2023-12-17 17:05:05 +0200

    add cron logic but with external package

--- src/functions/handlers/sendingEmail.ts
+++ src/functions/handlers/sendingEmail.ts
@@ -1,9 +1,32 @@
 import { SES } from 'aws-sdk';
 import { SendEmailResponse } from 'aws-sdk/clients/ses';
+import schedule from 'node-schedule';
 
 const ses = new SES();
 
 export default async function sender(recipient: string, term: number): Promise<SendEmailResponse> {
+    if(term) {
+        schedule.scheduleJob(new Date(term), () => {
+            ses.sendEmail({
+                Source: 'ekt_1@ukr.net',
+                Destination: {
+                    ToAddresses: [recipient]
+                },
+                Message: {
+                    Body: {
+                        Text: {
+                            Charset: 'UTF-8',
+                            Data: 'Your URL item will be deleted soon'
+                        }
+                    },
+                    Subject: {
+                        Charset: 'UTF-8',
+                        Data: 'Expiration alert'
+                    }
+                }
+            }).promise();
+        });
+    }
     return ses.sendEmail({
         Source: 'ekt_1@ukr.net',
         Destination: {
